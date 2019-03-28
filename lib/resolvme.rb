ffbce21b64127cd872a75744288072dc7e01098c
Michele Sorcinelli <michele.sorcinelli@citizensadvice.org.uk>
2019-03-28 13:52:58 +0000

    Add doc for resolvme.rb

--- lib/resolvme.rb
+++ lib/resolvme.rb
@@ -1,20 +1,23 @@
-class ResolvmeError < StandardError;
-end
-
 require "resolvme/vault/client"
 require "resolvme/aws/cf_stack_output"
 require "resolvme/aws/acm_cert_arn"
 
+# Generic error
+class ResolvmeError < StandardError; end
+
+# Wrapper for {::Resolvme::Vault::Client#get_secret}
 def vault(path, key)
   @vault ||= ::Resolvme::Vault::Client.new
   @vault.get_secret(path, key)
 end
 
+# Wrapper for {::Resolvme::Aws::CloudformationStackOutput#get_stack_output}
 def cf_stack_output(stack, name, region = nil)
   @so ||= ::Resolvme::Aws::CloudformationStackOutput.new
   @so.get_stack_output(stack, name, region)
 end
 
+# Wrapper for {::Resolvme::Aws::AcmCertificateArn#acm_arn}
 def acm_cert_arn(domain, region = nil)
   acm = ::Resolvme::Aws::AcmCertificateArn.new
   acm.acm_arn(domain, region)
