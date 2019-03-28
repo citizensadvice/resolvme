1860ae2c3b9ee3203fb02411ccc9b01ce62dc948
Michele Sorcinelli <michele.sorcinelli@citizensadvice.org.uk>
2019-03-28 13:20:11 +0000

    Simplify Vault resolver

--- lib/resolvme/vault/client.rb
+++ lib/resolvme/vault/client.rb
@@ -4,67 +4,46 @@ require "vault"
 module Resolvme
   module Vault
     class Client
-      class VaultKeyNotFoundException < ::ResolvmeError; end
+      # Secret with the given path not found
+      class VaultSecretNotFound < ::ResolvmeError; end
+      # Field with the given key not found inside the secret
+      class VaultKeyNotFound < ::ResolvmeError; end
 
-      def get_secret(path, query)
-        query_keys = query.split("/").map(&:to_sym)
-        cache[path] ||= {}
-        # TODO what if we have a key in vault containing / ?
-        #   {
-        #    "foo/bar/jar" : 1
-        #  }
-        # This will be treated the same as
-        #  {
-        #   foo: { bar: { jar: 1 }}
-        #  }
-        val = cache[path][query]
-        return val unless val.nil?
-        cache[path][query] = vault_secret(path, query_keys)
+      # Read the secret at the provided path and return the value
+      # of the field with the provided key.
+      #
+      # @param path [String] secret path
+      # @param key [String] field key
+      # @raise [VaultSecretNotFound]
+      # @raise [VaultKeyNotFound]
+      # @return [Object] field value
+      def get_secret(path, key)
+        secret = fetch_secret(path)
+        raise VaultSecretNotFound,
+              "Secret #{path} not found" unless secret
+        value = secret.data[key.to_sym]
+        raise VaultKeyNotFound,
+              "Secret #{path} doesn't have this key: #{key}" unless value
+        value
+      end
+
+      # Initialize the object. The VAULT_GITHUB_TOKEN environment variable can
+      # be used to force authentication via Github token, this is not usually
+      # required if you've already authenticated via the Vault command line.
+      def initialize()
+        gh_token = ENV["VAULT_GITHUB_TOKEN"]
+        opts = {}
+        opts[:token] = ::Vault.auth.github(gh_token).auth.client_token if gh_token
+        @vault = ::Vault::Client.new(opts)
+        @cache = {}
       end
 
       private
 
-      def cache
-        @cache ||= {}
+      # Fetches the secret from the cache or from Vault if not cached.
+      def fetch_secret(path)
+        @cache[path] ||= @vault.logical.read(path)
       end
-
-      def vault
-        @vault ||= begin
-          gh_token = ENV.fetch("VAULT_GITHUB_TOKEN", false)
-          if gh_token
-            vault_token = Vault.auth.github(gh_token)
-            ::Vault::Client.new(address: ENV["VAULT_ADDR"], token: vault_token.auth.client_token)
-          else
-            # use ENV["VAULT_ADDR"] and ENV["VAULT_TOKEN"] if set
-            ::Vault::Client.new
-          end
-        end
-      end
-
-      def vault_data(path, options = {})
-        attempts = options.fetch(:attempts, 1)
-        vault.with_retries(::Vault::HTTPError, attempts: attempts) do |attempt, e|
-          warn "Received exception #{e} from Vault - attempt #{attempt}" if e
-          vault.logical.read(path)
-        end
-      end
-
-      def vault_secret(path, query = [], options = {})
-        secret = vault_data(path, options)
-        if secret.nil?
-          msg = "Path: #{path}"
-          msg += ", Keys: #{query.join(' -> ')}" unless query.empty?
-          msg += ". Called from #{caller[0]}"
-          raise VaultKeyNotFoundException, msg
-        end
-        return secret.data if query.empty?
-        val = secret.data.dig(*query)
-        if (val).nil?
-          raise VaultKeyNotFoundException, "Path: #{path}, query: #{query}"
-        end
-        val
-      end
-
     end
   end
 end
