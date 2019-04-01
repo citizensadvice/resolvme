7f8cdbd5aab8109ba91bb35a826fdb1428e15589
Michele Sorcinelli <michele.sorcinelli@citizensadvice.org.uk>
2019-04-01 14:56:24 +0100

    Add some doc

--- lib/resolvme/aws/aws_client_options.rb
+++ lib/resolvme/aws/aws_client_options.rb
@@ -2,16 +2,11 @@
 module Resolvme
   module Aws
     module AwsClientOptions
-
       attr_writer :stub_responses
 
-      def aws_client_instance(service_id, region)
-        client_opts = {region: region, stub_responses: stub_responses, retry_limit: 10}.
-            delete_if{|_k,v| v.nil?}
-        klass = ::Aws.const_get "::Aws::#{service_id}::Client"
-        klass.new(client_opts)
-      end
-
+      # Return an AWS client for the given service id
+      # @param [Symbol] Service ID (e.g. :EC2 or :CloudFormation)
+      # @param [String] AWS Region (optional)
       def aws_client(service_id, region = nil)
         regional_client_id = "#{region}_#{service_id}"
         regional_clients[regional_client_id] ||= aws_client_instance(service_id, region)
@@ -24,10 +19,19 @@ module Resolvme
 
       protected
 
+      # @return [Array] List of AWS clients
       def regional_clients
         @_regional_clients_ ||= {}
       end
 
+      private
+
+      def aws_client_instance(service_id, region)
+        client_opts = {region: region, stub_responses: stub_responses, retry_limit: 10}.
+            delete_if{|_k,v| v.nil?}
+        klass = ::Aws.const_get "::Aws::#{service_id}::Client"
+        klass.new(client_opts)
+      end
     end
   end
 end
--- lib/resolvme/aws/cf_stack_output.rb
+++ lib/resolvme/aws/cf_stack_output.rb
@@ -7,12 +7,24 @@ module Resolvme
     class CloudformationStackOutput
       include AwsClientOptions
 
+      # Returns a single stack output.
+      #
+      # @param stack_name [String] The stack name
+      # @param output_key [String] The output name
+      # @param region [String] AwS region
+      # @return [String] The output value
       def get_stack_output(stack_name, output_key, region = nil)
         output = get_stack_outputs(stack_name, region).find { |o| o.output_key == output_key }
         raise ResolvmeError, "Stack output #{stack_name}/#{output_key} not found" unless output
         output.output_value
       end
 
+      # Returns a list of outputs for a stack.
+      #
+      # @param stack_name [String] The stack name
+      # @param output_key [String] The output name
+      # @param region [String] AwS region
+      # @return [Array<Aws::CloudFormation::Types::Output>] Stack outputs
       def get_stack_outputs(stack_name, stack_region = nil)
         stack = aws_client(:CloudFormation, stack_region).describe_stacks(stack_name: stack_name).stacks.first
         raise ResolvmeError, "Stack #{stack_name} not found" unless stack
