44148aa81edeb9f2e5d0e34525512f739d3680ce
Michele Sorcinelli <michele.sorcinelli@citizensadvice.org.uk>
2019-04-01 15:46:32 +0100

    Fix typo

--- lib/resolvme/aws/cf_stack_output.rb
+++ lib/resolvme/aws/cf_stack_output.rb
@@ -12,7 +12,7 @@ module Resolvme
       #
       # @param stack_name [String] The stack name
       # @param output_key [String] The output name
-      # @param region [String] AwS region
+      # @param region [String] AWS region
       # @return [String] The output value
       def get_stack_output(stack_name, output_key, region = nil)
         output = get_stack_outputs(stack_name, region).find { |o| o.output_key == output_key }
@@ -24,7 +24,7 @@ module Resolvme
       #
       # @param stack_name [String] The stack name
       # @param output_key [String] The output name
-      # @param region [String] AwS region
+      # @param region [String] AWS region
       # @return [Array<Aws::CloudFormation::Types::Output>] Stack outputs
       def get_stack_outputs(stack_name, stack_region = nil)
         stack = aws_client(:CloudFormation, stack_region).describe_stacks(stack_name: stack_name).stacks.first
