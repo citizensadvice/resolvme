881b7cdd1670fe667cff40c1b024d2b1aa4020f6
Zhelyan Panchev <zhelyan.panchev@citizensadvice.org.uk>
2019-09-17 08:51:25 +0100

    fix(cf_stack_output): results not cached

--- lib/resolvme/aws/cf_stack_output.rb
+++ lib/resolvme/aws/cf_stack_output.rb
@@ -1,7 +1,8 @@
 # frozen_string_literal: true
-require "aws-sdk-cloudformation"
-require "resolvme/error"
-require "resolvme/aws/aws_client_options"
+
+require 'aws-sdk-cloudformation'
+require 'resolvme/error'
+require 'resolvme/aws/aws_client_options'
 
 module Resolvme
   module Aws
@@ -11,21 +12,21 @@ module Resolvme
       class OutputNotFoundError < ResolvmeError; end
 
       # Initialize the object instance
-      # @param region [String] AWS region
       # @return [Resolvme::Aws::CloudformationStackOutput]
-      def initialize(region = nil)
+      def initialize
         @cache = {}
-        @region = region
       end
 
       # Returns a single stack output.
       #
       # @param stack_name [String] The stack name
       # @param output_key [String] The output name
+      # @param region [String] AWS region
       # @return [String] The output value
-      def get_stack_output(stack_name, output_key)
-        output = get_stack_outputs(stack_name).find { |o| o.output_key == output_key }
+      def get_stack_output(stack_name, output_key, region = nil)
+        output = get_stack_outputs(stack_name, region).find { |o| o.output_key == output_key }
         raise OutputNotFoundError, "Stack output #{stack_name}/#{output_key} not found" unless output
+
         output.output_value
       end
 
@@ -35,8 +36,10 @@ module Resolvme
       # @param stack_name [String] The stack name
       # @param output_key [String] The output name
       # @return [Array<Aws::CloudFormation::Types::Output>] Stack outputs
-      def get_stack_outputs(stack_name)
-        @cache[stack_name] ||= aws_client(:CloudFormation, @region).describe_stacks(stack_name: stack_name).stacks.first.outputs
+      def get_stack_outputs(stack_name, region)
+        @cache["#{stack_name}_#{region}"] ||= aws_client(:CloudFormation, region)
+                                              .describe_stacks(stack_name: stack_name)
+                                              .stacks.first.outputs
       end
     end
   end
